788bc28b8e75514b063e2756977969bb8f04bde1
DESKTOP-AB8J0V3\PC_User <y24340216@gmail.com>
2025-10-02 02:52:25 +0900

    ボタンの追加

--- Assets/TechC/Scripts/MainScene/Player/PlayerInputManager.cs
+++ Assets/TechC/Scripts/MainScene/Player/PlayerInputManager.cs
@@ -1,41 +1,46 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 using System;
 
 namespace TechC.Main.Player
 {
-    /// <summary>
-    /// プレイヤー入力を管理するクラス
-    /// </summary>
     public class PlayerInputManager : MonoBehaviour
     {
+        [SerializeField] private Button leftButton;
+        [SerializeField] private Button rightButton;
+
         public event Action OnLeftInputStarted;
         public event Action OnLeftInputCanceled;
         public event Action OnRightInputStarted;
         public event Action OnRightInputCanceled;
 
-        public void OnMoveLeft(InputAction.CallbackContext context)
+        private void Awake()
         {
-            if (context.started)
-            {
-                OnLeftInputStarted?.Invoke();
-            }
-            else if (context.canceled)
-            {
-                OnLeftInputCanceled?.Invoke();
-            }
+            // EventTrigger で PointerDown / PointerUp を追加
+            AddEventTrigger(leftButton, OnLeftPressed, OnLeftReleased);
+            AddEventTrigger(rightButton, OnRightPressed, OnRightReleased);
         }
 
-        public void OnMoveRight(InputAction.CallbackContext context)
+        private void AddEventTrigger(Button button, Action onDown, Action onUp)
         {
-            if (context.started)
-            {
-                OnRightInputStarted?.Invoke();
-            }
-            else if (context.canceled)
-            {
-                OnRightInputCanceled?.Invoke();
-            }
+            var trigger = button.gameObject.GetComponent<EventTrigger>();
+            if (trigger == null) trigger = button.gameObject.AddComponent<EventTrigger>();
+
+            // PointerDown
+            var entryDown = new EventTrigger.Entry { eventID = EventTriggerType.PointerDown };
+            entryDown.callback.AddListener((data) => onDown());
+            trigger.triggers.Add(entryDown);
+
+            // PointerUp
+            var entryUp = new EventTrigger.Entry { eventID = EventTriggerType.PointerUp };
+            entryUp.callback.AddListener((data) => onUp());
+            trigger.triggers.Add(entryUp);
         }
+
+        private void OnLeftPressed()  => OnLeftInputStarted?.Invoke();
+        private void OnLeftReleased() => OnLeftInputCanceled?.Invoke();
+        private void OnRightPressed() => OnRightInputStarted?.Invoke();
+        private void OnRightReleased() => OnRightInputCanceled?.Invoke();
     }
 }
